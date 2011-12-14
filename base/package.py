61e3f30f90761138dd3fee0eb7f511640f8dabc9
Aaron Small <aaron.small.99a@gmail.com>
2011-12-14 09:45:25 -0500

    Don't try to download source again after it has been downloaded.
    Add build commands

--- base/package.py
+++ base/package.py
@@ -3,39 +3,63 @@ import os
 import sys
 import libxml2
 
-def gotodir(package):
-    os.chdir(package)
-
-def leavedir(package):
-    os.chdir("../..")
-
 def mkworkdir():
+    try:
+        os.mkdir("src")
+    except OSError as e:
+        pass
+
     try:
         os.mkdir("obj")
     except OSError as e:
         pass
-    os.chdir("obj")
 
 def downloadpackage(package):
     doc = libxml2.parseFile("../package.xml")
     ctxt = doc.xpathNewContext()
-    res = ctxt.xpathEval("/port/download/command/node()")
+
+    # Check if the source is already available
+    res = ctxt.xpathEval("/port/download/result/node()")
+    if os.path.exists(str(res[0])):
+        print "Source for '%s' already downloaded" % package
+    else:
+        res = ctxt.xpathEval("/port/download/command/node()")
+        for i in res:
+            os.system(str(i))
+    doc.freeDoc()
+    ctxt.xpathFreeContext()
+
+def buildpackage(package):
+    doc = libxml2.parseFile("../package.xml")
+    ctxt = doc.xpathNewContext()
+
+    # Check if the source is already available
+    print os.getcwd()
+    res = ctxt.xpathEval("/port/build/result/node()")
+    res = ctxt.xpathEval("/port/build/command/node()")
     for i in res:
         os.system(str(i))
     doc.freeDoc()
     ctxt.xpathFreeContext()
 
 def build(package):
-    gotodir(package)
+    os.chdir(package)
     mkworkdir()
+    os.chdir("src")
     downloadpackage(package)
-    leavedir(package)
+    buildpackage(package)
+    os.chdir("..")
+    os.chdir("..")
 
+# Go to ports directory
+os.chdir(os.path.dirname(sys.argv[0]))
+os.chdir("../ports")
+
+# See what is there
 if( len(sys.argv) > 1 ):
     packages = [ sys.argv[1] ]
 else:
-    packages = os.listdir("../ports")
+    packages = filter(lambda x: x[0]!='.', os.listdir("../ports"))
 
 for package in packages:
-    os.chdir("../ports")
     build(package)
