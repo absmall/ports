2213af70ae44124bbc6cf07348440707dca40e8e
Aaron Small <aaron.small.99a@gmail.com>
2011-12-14 09:21:37 -0500

    Tabs to spaces

--- base/package.py
+++ base/package.py
@@ -4,35 +4,38 @@ import sys
 import libxml2
 
 def gotodir(package):
-	os.chdir(package)
+    os.chdir(package)
 
 def leavedir(package):
-	os.chdir("../..")
+    os.chdir("../..")
 
 def mkworkdir():
-	#os.mkdir("obj")
-	os.chdir("obj")
+    try:
+        os.mkdir("obj")
+    except OSError as e:
+        pass
+    os.chdir("obj")
 
 def downloadpackage(package):
-	doc = libxml2.parseFile("../package.xml")
-	ctxt = doc.xpathNewContext()
-	res = ctxt.xpathEval("/port/download/command/node()")
-	for i in res:
-		os.system(str(i))
-	doc.freeDoc()
-	ctxt.xpathFreeContext()
+    doc = libxml2.parseFile("../package.xml")
+    ctxt = doc.xpathNewContext()
+    res = ctxt.xpathEval("/port/download/command/node()")
+    for i in res:
+        os.system(str(i))
+    doc.freeDoc()
+    ctxt.xpathFreeContext()
 
 def build(package):
-	gotodir(package)
-	mkworkdir()
-	downloadpackage(package)
-	leavedir(package)
+    gotodir(package)
+    mkworkdir()
+    downloadpackage(package)
+    leavedir(package)
 
 if( len(sys.argv) > 1 ):
-	packages = [ sys.argv[1] ]
+    packages = [ sys.argv[1] ]
 else:
-	packages = os.listdir("../ports")
+    packages = os.listdir("../ports")
 
 for package in packages:
-	os.chdir("../ports")
-	build(package)
+    os.chdir("../ports")
+    build(package)
